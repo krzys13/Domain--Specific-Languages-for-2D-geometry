e5c52e639918fb09cd27d455f720628e98ecec19
Allen <gumek09@gmail.com>
2026-03-26 14:51:26 +0100

    Add main class and visitor class

--- src/interpreter/Start.java
+++ src/interpreter/Start.java
@@ -1,30 +1,28 @@
 package interpreter;
-
 import grammar.*;
 import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.tree.ParseTree;
-
-import java.io.IOException;
+import org.antlr.v4.runtime.tree.*;
 
 public class Start {
-    public static void main(String[] args) {
-        CharStream inp = null;
-        try {
-            inp = CharStreams.fromFileName("we.first");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-//        CharStream inp = CharStreams.fromString("1+2*3-(4+5)","wejście");
-//        CharStream inp = CharStreams.fromStream(System.in);
+    public static void main(String[] args) throws Exception {
+        // create a CharStream that reads from standard input
+        CharStream input = CharStreams.fromStream(System.in);
 
-        firstLexer lex = new firstLexer(inp);
-        CommonTokenStream tokens = new CommonTokenStream(lex);
-        firstParser par = new firstParser(tokens);
+        // create a lexer that feeds off of input CharStream
+        GeoLangLexer lexer = new GeoLangLexer(input);
 
-        ParseTree tree = par.prog();
+        // create a buffer of tokens pulled from the lexer
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
 
-        CalculateVisitor v = new CalculateVisitor(inp,tokens);
-        Integer res = v.visit(tree);
-//        System.out.printf("Wynik: %d\n", res);
+        // create a parser that feeds off the tokens buffer
+        GeoLangParser parser = new GeoLangParser(tokens);
+
+        // start parsing at the program rule
+        ParseTree tree = parser.program();
+        // System.out.println(tree.toStringTree(parser));
+
+        // create a visitor to traverse the parse tree
+        Kolorowy visitor = new Kolorowy();
+        System.out.println(visitor.visit(tree));
     }
 }
