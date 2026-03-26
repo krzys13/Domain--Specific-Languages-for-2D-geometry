80d0964b2df8987dc507c32d3f87582918c33838
Allen <gumek09@gmail.com>
2026-03-26 21:26:25 +0100

    add mising blank visitor class

---
+++ src/interpreter/Kolorowy.java
@@ -0,0 +1,14 @@
+package interpreter;
+
+
+import SymbolTable.GlobalSymbols;
+import SymbolTable.LocalSymbols;
+import grammar.*;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.misc.Interval;
+
+
+class Kolorowy extends GeoLangParserBaseVisitor<Integer>{
+}
