58ea521d3c02edb334d7a6a9d17eebd139bde47c
darG09 <154283479+darG09@users.noreply.github.com>
2026-04-25 16:15:08 +0200

    render_patch
    generowanie kształtów teraz odbywa się przy użyciu metody render() dla każdej figury osobno

---
+++ CircleType.java
@@ -0,0 +1,59 @@
+package interpreter.variables;
+
+import interpreter.drawable.DrawCollector;
+import interpreter.drawable.DrawableCircle;
+
+public class CircleType implements VarType {
+
+    private final VarTypeEnum type;
+
+    public PointType c;// center of cirlce
+    public FloatType r; // radius
+
+    public CircleType(PointType s, FloatType r) {
+        this.type = VarTypeEnum.CIRCLE;
+        this.c = s;
+        this.r = r;
+    }
+
+    @Override
+    public VarTypeEnum getType() {
+        return this.type;
+    }
+
+    @Override
+    public VarType getField(String fieldName) {
+        switch (fieldName) {
+            case "c":
+                return (this.c);
+            case "r":
+                return (this.r);
+            default:
+                throw new RuntimeException("CIRCLE has no field: " + fieldName);
+        }
+    }
+
+    @Override
+    public VarType setField(String fieldName, VarType value) {
+        switch (fieldName) {
+            case "c": return this.c = (PointType) value;
+            case "r": return this.r = (FloatType) value;
+            default: throw new RuntimeException("CIRCLE has no field: " + fieldName);
+        }
+    }
+
+    @Override
+    public VarType getMethod(String methodName, VarType... args) {
+        if ("render".equals(methodName)) {
+            DrawCollector.add(new DrawableCircle(
+                    this.c.x.value, this.c.y.value, this.r.value));
+            return this; // zwracamy siebie, aby można było łańcuchować
+        }
+        throw new RuntimeException("CIRCLE has no method: " + methodName);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + c + ", " + r + ")";
+    }
+}
---
+++ LineType.java
@@ -0,0 +1,63 @@
+package interpreter.variables;
+
+
+import interpreter.drawable.DrawCollector;
+import interpreter.drawable.DrawableLine;
+
+public class LineType implements VarType {
+    private final VarTypeEnum type;
+
+    public PointType p1, p2;
+
+    public LineType(PointType p1, PointType p2) {
+        this.type = VarTypeEnum.LINE;
+        this.p1 = p1;
+        this.p2 = p2;
+
+    }
+
+    @Override
+    public VarTypeEnum getType() {
+        return this.type;
+    }
+
+
+
+
+    @Override
+    public VarType getField(String fieldName) {
+        switch (fieldName) {
+            case "p1":
+                return (this.p1);
+            case "p2":
+                return (this.p2);
+            default:
+                throw new RuntimeException("LINE has no field: " + fieldName);
+        }
+    }
+
+    @Override
+    public VarType setField(String fieldName, VarType value) {
+        switch (fieldName) {
+            case "p1": return this.p1 = (PointType) value;
+            case "p2": return this.p2 = (PointType) value;
+            default: throw new RuntimeException("LINE has no field: " + fieldName);
+        }
+    }
+
+    @Override
+    public VarType getMethod(String methodName, VarType... args) {
+        if ("render".equals(methodName)) {
+            DrawCollector.add(new DrawableLine(
+                    this.p1.x.value, this.p1.y.value,
+                    this.p2.x.value, this.p2.y.value));
+            return this;
+        }
+        throw new RuntimeException("LINE has no method: " + methodName);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + p1 + ", " + p2 + ")";
+    }
+}
