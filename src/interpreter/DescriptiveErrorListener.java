b4f35ae69a3720cb0e928cbb3a5c4ac5c704ee2d
Dorota <dm.ptasznik@gmail.com>
2026-04-26 23:35:46 +0200

    feat: add coordinate grid, error console and UI styling

--- src/interpreter/DescriptiveErrorListener.java
+++ src/interpreter/DescriptiveErrorListener.java
@@ -1,18 +1,22 @@
 package interpreter;
 
+import javafx.scene.control.TextArea;
 import org.antlr.v4.runtime.BaseErrorListener;
 import org.antlr.v4.runtime.RecognitionException;
 import org.antlr.v4.runtime.Recognizer;
 
 // Pomocnicza klasa do wyświetlania błędów składniowych
 public class DescriptiveErrorListener extends BaseErrorListener {
+    private final TextArea output;
+
+    public DescriptiveErrorListener(TextArea output) {
+        this.output = output;
+    }
     @Override
     public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol,
                             int line, int charPositionInLine, String msg, RecognitionException e) {
-        String sourceName = recognizer.getInputStream().getSourceName();
-        if (!sourceName.isEmpty()) {
-            sourceName = String.format("%s:%d:%d: ", sourceName, line, charPositionInLine);
-        }
-        System.err.println(sourceName + "błąd składni: " + msg);
+        String errorMsg = String.format("Linia %d:%d - %s\n", line, charPositionInLine, msg);
+
+        output.appendText(errorMsg);
     }
 }
